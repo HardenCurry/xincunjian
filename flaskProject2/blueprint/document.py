0e0e4a4f9ef08bff2fd94e51e21bb97f4227150f
Curry <sekran22@naver.com>
2023-05-30 01:40:13 +0800

    记录最终

--- flaskProject2/blueprint/document.py
+++ flaskProject2/blueprint/document.py
@@ -11,9 +11,18 @@ bp = Blueprint("document", __name__, url_prefix="/document")
 def document():
     user = session.get('zhanghao')
     time = request.args.get('time')
-    session['time'] = time
+    print(time)
     if not time:
-        time = '早'
+        # 不是按button
+        if not session.get('time'):
+            # 第一次进入
+            time = '早'
+            session['time'] = time
+        else:
+            time = session.get('time')
+    else:
+        session['time'] = time
+
     fnames = []
     fenergies = []
     fnums = []
@@ -24,24 +33,26 @@ def document():
     if not user:
         flash('登录才能使用该功能')
     else:
+        # all_energy 计算总卡路里
         doc0 = Document.query.filter(Document.zhanghao == user).all()
         for i in doc0:
-            fnums.append(i.fnum)
-            weights.append(i.weight)
-            food0 = Food.query.filter(Food.fnum == i.fnum).first()
-            all_energy += int(food0.energy)
+            food = Food.query.filter(Food.fnum == i.fnum).first()
+            all_energy += (round(int(food.energy) * i.weight * 0.01))
 
+        # 符合time的记录
         doc = Document.query.filter(Document.zhanghao == user).filter(Document.time == time).all()
         for i in doc:
+            fnums.append(i.fnum)
+            weights.append(i.weight)
             food = Food.query.filter(Food.fnum == i.fnum).first()
-            sum_energy += int(food.energy)
+            sum_energy += round(int(food.energy) * i.weight * 0.01)
             fnames.append(food.fname)
             fimgs.append(food.img)
-            fenergies.append(food.energy)
-    search=session.get('search')
-    searchtype=session.get('searchtype')
-    type=session.get('type2')
-    paixu=session.get('paixu')
+            fenergies.append(round(int(food.energy) * i.weight * 0.01))
+    search = session.get('search')
+    searchtype = session.get('searchtype')
+    type = session.get('type')
+    paixu = session.get('paixu')
     if search:
         session.pop('search')
     if searchtype:
@@ -61,12 +72,17 @@ def document1(fnum):
     time = session.get('time')
     if not time:
         time = '早'
+    session['time']=time
     if not user:
         flash('登录才能使用该功能')
     else:
         date = datetime.datetime.now()
         x = '{:%Y-%m-%d}'.format(date)
-        print(x)
+        doc0 = Document.query.filter(Document.zhanghao == user).filter(Document.time == time).all()
+        for i in doc0:
+            if i.fnum == fnum:
+                flash('此食品已在记录中，不能重复添加')
+                return redirect(url_for('document.document'))
         doc = Document(zhanghao=user, fnum=fnum, weight=100, date=x, time=time)
         db.session.add(doc)
         db.session.commit()
@@ -78,7 +94,9 @@ def document1(fnum):
 def del_doc():
     fnum = request.form['fnum_btn']
     user = session.get('zhanghao')
-    del_fnum = Document.query.filter(Document.zhanghao == user).filter(Document.fnum == fnum).first()
+    time = session.get('time')
+    del_fnum = Document.query.filter(Document.zhanghao == user).filter(Document.fnum == fnum).filter(
+        Document.time == time).first()
     db.session.delete(del_fnum)
     db.session.commit()
     return redirect(url_for('document.document'))
@@ -88,8 +106,8 @@ def del_doc():
 def update_weight():
     fnum = request.form['fnum_btn2']
     weight = request.form[f'{fnum}']
-    print(weight, fnum)
-    doc=Document.query.filter(Document.fnum==fnum).first()
-    doc.weight=weight
+    time = session.get('time')
+    doc = Document.query.filter(Document.fnum == fnum).filter(Document.time == time).first()
+    doc.weight = weight
     db.session.commit()
     return redirect(url_for('document.document'))
